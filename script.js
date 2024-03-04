fe180c2628b8a34a8f20012f51e58b5d33c05b42
Isabelli Marques <101362239+isabellimrqs@users.noreply.github.com>
2024-03-04 11:04:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// // 1) Dobro 
+// let numeros = [1,2,3,4,5];
+
+// for (let i = 0; i < numeros.length; i++){
+//     numeros[i] *= 2;
+// }
+
+// console.log(numeros)
+
+// //2) Média Aritmética
+// let vetores = [2, 4, 5, 4, 3, 7, 8, 9]
+
+// document.querySelector("#result").innerHTML = `
+//     <h2>A Média dos vetores é: ${somar()/vetores.length}
+// `
+// function somar() {
+//   let soma = 0
+//   for(let i in vetores) {
+//     soma += vetores[i]
+//   }
+//   return soma
+// }
+
+// //3) Idade
+// let idades = [15, 16, 20, 25, 65, 45, 41, 50, 70, 9, 10, 12, 18, 19, 28, 34, 24, 44, 55, 13];
+
+// function somar() {
+//   let soma = 0;
+//   for (let i in idades) {
+//     soma += idades[i];
+//   }
+//   return soma;
+// }
+
+// let media = somar() / idades.length;
+// console.log(media);
+
+// let idadesAcimaDaMedia = [];
+// for (let i in idades) {
+//   if (idades[i] > media) {
+//     idadesAcimaDaMedia.push(idades[i]);
+//   }
+// }
+
+// console.log("As idades acima da média são: ", idadesAcimaDaMedia);
+
+// //4) Par ou ímpar
